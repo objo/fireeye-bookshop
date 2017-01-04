0faca7c23cb9b90415c339f0ef64e456e37b2dc1
Joe OBrien <joe@objo.com>
2017-01-04 14:19:35 -0500

    getting price calculations to work correctly / added a to_f to incomming prices in case strings are used

--- lib/bookshop/inventory.rb
+++ lib/bookshop/inventory.rb
@@ -1,9 +1,10 @@
 module Bookshop
   class Inventory
 
-    def initialize(books, *args)
+    def initialize(books, magazines, *args)
       @inventory = {}
-      add_books(books)
+      add_books(books) if books
+      add_magazines(magazines) if magazines
     end
 
     def calculate_price(identifier)
@@ -22,21 +23,39 @@ module Bookshop
     def add_books(book_array)
       book_array.each { |book| @inventory[book[:identifier]] = Book.new(book) }
     end
-  end
-  
-  class Book
 
+    def add_magazines(magazine_array)
+      magazine_array.each { |magazine| @inventory[magazine[:identifier]] = Magazine.new(magazine) }
+    end
+  end
+
+  class Book
     attr_accessor :identifier, :base_price
     STORE_FEE = 4.75
-
+    
     def initialize(params)
       @identifier = params[:identifier]
-      @base_price = params[:base_price]
+      @base_price = params[:base_price].to_f
     end
 
     def price
       (base_price + STORE_FEE) * 1.05
     end
+  end
+
+  class Magazine
+    attr_accessor :identifier, :base_price
+    STORE_FEE = 1.35
+    
+    def initialize(params)
+      @identifier = params[:identifier]
+      @base_price = params[:base_price].to_f
+    end
+
+    def price
+      (base_price + STORE_FEE) * 1.05
+    end
+
   end
   
 end
--- test/test_inventory.rb
+++ test/test_inventory.rb
@@ -37,9 +37,42 @@ class TestInventory < Test::Unit::TestCase
     assert_equal book.identifier, book.identifier
   end
 
+  def test_find_returns_magazine_objects
+    identifier = "1111"
+    base_price = "5.00"
+    
+    magazines = [{identifier: identifier, base_price: base_price}]
+    inventory = Bookshop::Inventory.new(nil, magazines, nil)
+    magazine = inventory.find(identifier)
+
+    assert_equal magazine.class, Bookshop::Magazine
+    assert_equal magazine.base_price, magazine.base_price
+    assert_equal magazine.identifier, magazine.identifier
+  end
+
   def test_calculate_correctly_calculates_book_price
+    identifier = "1111"
+    base_price = 10.00
+    
+    books = [{identifier: identifier, base_price: base_price}]
+    inventory = Bookshop::Inventory.new(books, nil, nil)
+
     expected_book_price = 15.4875
 
-    assert_equal expected_book_price, @inventory.calculate_price(@book_identifier)
+    assert_equal expected_book_price, inventory.calculate_price(identifier)
   end
+
+  def test_calculate_correctrly_calculates_magazine_price
+    identifier = "1111"
+    base_price = 5.50
+    
+    magazines = [{identifier: identifier, base_price: base_price}]
+    inventory = Bookshop::Inventory.new(nil, magazines, nil)
+
+    expected_magazine_price = 7.1925
+    
+    assert_equal expected_magazine_price, inventory.calculate_price(identifier)
+  end
+
+
 end
